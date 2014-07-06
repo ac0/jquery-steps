72e257a622c27029a09adeb8dac0ebc565028410
ac <ac0zap@gmail.com>
2014-07-06 18:41:47 +0100

    Redistribute under GPL, compatible under the terms of existing MIT license

--- build/jquery.steps.js
+++ build/jquery.steps.js
@@ -1,7 +1,30 @@
 /*! 
+ * jQuery Steps v1.0.7.f.1-SNAPSHOT - 6 July 2014
+ * Copyright (c) 2014 AC
+ * Licensed under GPL
+ *
  * jQuery Steps v1.0.7 - 05/07/2014
  * Copyright (c) 2014 Rafael Staib (http://www.jquery-steps.com)
- * Licensed under MIT http://www.opensource.org/licenses/MIT
+ *
+ * Previously licensed under MIT http://www.opensource.org/licenses/MIT
+ *     - Not applicable for any new code, but text retained as previous_LICENSE.txt
+ *
+ *   
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * see COPYING for full text. 
+ * 
  */
 ;(function ($, undefined)
 {
